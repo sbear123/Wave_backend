3bd6ba11a4f32145724a15befc94d48529e228e9
bees3030 <bees_3030@naver.com>
2020-09-02 19:45:51 +0900

    장르빈, 유저빈 수정

--- src/bean/UserBean.java
+++ src/bean/UserBean.java
@@ -2,33 +2,50 @@ package bean;
 
 public class UserBean {
 	
-	private String userId;
+	private String userid;
 	private String password;
 	private String name;
+	private String favorite;
+	private String email;
 	
 	public UserBean() {}
 	
-	public UserBean(String userId, String password) {
-		this.userId = userId;
+	public UserBean(String userid, String password) {
+		this.userid = userid;
 		this.password = password;
 	}
-	public UserBean(String userId, String password, String name) {
-		this.userId = userId;
+	
+	public UserBean(String userid, String password, String name, String favorite) {
+		this.userid = userid;
 		this.password = password;
 		this.name = name;
+		this.favorite = favorite;
 	}
-	public String getUserId() {
-		return userId;
+	
+	public UserBean(String userid, String password, String name, String favorite, String email) {
+		this.userid = userid;
+		this.password = password;
+		this.name = name;
+		this.favorite = favorite;
+		this.email = email;
 	}
-	public void setUserId(String userId) {
-		this.userId = userId;
+
+	public String getUserid() {
+		return userid;
 	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getName() {
 		return name;
 	}
@@ -36,4 +53,21 @@ public class UserBean {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getFavorite() {
+		return favorite;
+	}
+
+	public void setFavorite(String favorite) {
+		this.favorite = favorite;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
