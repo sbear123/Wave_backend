39ed70969396d733319ad1f60783579d08b700a5
sbear123 <sbear123@naver.com>
2020-09-23 19:21:09 +0900

    [UPDATE] searchjsonaction완성

--- src/wave/action/api/SearchJsonAction.java
+++ src/wave/action/api/SearchJsonAction.java
@@ -1,5 +1,33 @@
 package wave.action.api;
 
-public class SearchJsonAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+import com.google.gson.Gson;
+
+import bean.SearchDBBean;
+import bean.ResultBean;
+import bean.SearchBean;
+import bean.UserBean;
+import wave.action.Action;
+
+public class SearchJsonAction implements Action {
+
+	@Override
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("UTF-8");
+		Gson gson = new Gson();
+		SearchBean result = new SearchBean();
+		
+		// input
+		String str = IOUtils.toString(request.getReader());
+		String requestKeyword = gson.toJson(str); 
+		
+		result = SearchDBBean.getInstance().search(requestKeyword);
+		
+		return gson.toJson(result, SearchBean.class);
+	}
 
 }
