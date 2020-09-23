3e657a8d486df2c54dd7432abceed9b6a86e9219
sbear123 <sbear123@naver.com>
2020-09-23 19:47:27 +0900

    [ADD] KeywordBean

---
+++ src/bean/KeywordBean.java
@@ -0,0 +1,19 @@
+package bean;
+
+public class KeywordBean {
+	private String keyword;
+	
+	public KeywordBean(){}
+	
+	public KeywordBean(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+}
