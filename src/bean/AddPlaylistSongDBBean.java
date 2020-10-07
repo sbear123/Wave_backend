93c714acea57b4b17d4f6b619d5e8a06b4573787
bees3030 <bees_3030@naver.com>
2020-10-07 16:49:16 +0900

    커밋

---
+++ src/bean/AddPlaylistSongDBBean.java
@@ -0,0 +1,36 @@
+package bean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddPlaylistSongDBBean extends CommonDBBean{
+	//Singleton
+	private static AddPlaylistSongDBBean instance = new AddPlaylistSongDBBean();
+	private AddPlaylistSongDBBean() {}
+	public static AddPlaylistSongDBBean getInstance() {
+		return instance;
+	}
+	
+	public int add(PListBean plist) {
+		int result = 0;
+		Connection conn = getConnection();
+		if(conn==null) return 0;
+		System.out.println("conn");
+		
+		String sql = "insert into playlistsong(playlistid, songid) values (?,?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, plist.getPlaylistid());
+			pstmt.setInt(2, plist.getSongid());
+			result = pstmt.executeUpdate();
+			if(pstmt!=null) pstmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		closeConnection(conn);
+		return result;
+	}
+}
--- src/wave/action/api/AddPlaylistSongJsonAction.java
+++ src/wave/action/api/AddPlaylistSongJsonAction.java
@@ -1,5 +1,34 @@
 package wave.action.api;
 
-public class AddPlaylistSongJsonAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+
+import com.google.gson.Gson;
+
+import bean.AddPlaylistSongDBBean;
+import bean.PListBean;
+import bean.ResultBean;
+import wave.action.Action;
+
+public class AddPlaylistSongJsonAction implements Action{
+
+	@Override
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("UTF-8");
+		Gson gson = new Gson();
+		ResultBean result = new ResultBean();
+		
+		// input
+		String str = IOUtils.toString(request.getReader());
+		PListBean requestList = gson.fromJson(str, PListBean.class); 
+		
+		System.out.println(requestList.getPlaylistid() + requestList.getSongid());
+		int count = AddPlaylistSongDBBean.getInstance().add(requestList);
+		if(count==1)
+			result.result="ok"; 
+		
+		return gson.toJson(result, ResultBean.class);
+	}
 }
