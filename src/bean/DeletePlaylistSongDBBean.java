e7b46709cf00394a1613699d048c7a19b030deaa
bees3030 <bees_3030@naver.com>
2020-10-07 18:05:50 +0900

    플레이 리스트 노래 삭제

---
+++ src/bean/DeletePlaylistSongDBBean.java
@@ -0,0 +1,37 @@
+package bean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeletePlaylistSongDBBean extends CommonDBBean {
+
+	private static DeletePlaylistSongDBBean instance = new DeletePlaylistSongDBBean();
+	private DeletePlaylistSongDBBean() {}
+	public static DeletePlaylistSongDBBean getInstance() {
+		return instance;
+	}
+
+	public int DeleteSong(int playlistid, int songid) {
+
+		int result = 0;
+		Connection conn = getConnection();
+		if(conn==null) return 0;
+
+		String sql = "delete from playlistsong where playlistid = "+playlistid+" And songid ="+ songid;
+
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			result = pstmt.executeUpdate();
+
+			if(pstmt!=null) pstmt.close();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		closeConnection(conn);
+		return result;
+	}
+}
